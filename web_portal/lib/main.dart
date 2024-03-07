64d255642dae073cc3f002288cae289191f63b2f
Schreezer <96696271+Schreezer@users.noreply.github.com>
2024-03-07 22:37:15 +0530

    working

--- web_portal/lib/main.dart
+++ web_portal/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:web_portal/providers/userProvider.dart';
 import 'package:web_portal/screens/home_screen.dart';
 import 'package:web_portal/screens/login_screen.dart';
 import 'package:web_portal/screens/profile_screen.dart';
 import 'package:web_portal/screens/register_screen.dart';
 import 'package:web_portal/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -17,33 +18,45 @@ void main() async {
     storageBucket: 'billmanagement-99681.appspot.com',
   ));
   // await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  var initial_route= '/';
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.dark, // Set the theme brightness to dark
-        textTheme: const TextTheme(
-          bodyText1:
-              TextStyle(color: Colors.white), // Set the text color to white
+    @override
+    void initState() {
+      // Check if the user is already logged in
+      UserProvider userProvider = Provider.of<UserProvider>(context, listen: false);
+      userProvider.refreshUser(true);
+    }
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          brightness: Brightness.dark, // Set the theme brightness to dark
+          textTheme: const TextTheme(
+            bodyText1:
+                TextStyle(color: Colors.white), // Set the text color to white
+          ),
         ),
+        initialRoute: initial_route,
+        routes: {
+          '/': (context) => const WelcomeScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/register': (context) => const RegistrationScreen(),
+          '/home': (context) => const HomeScreen(),
+          '/profile': (context) => const ProfileScreen(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const WelcomeScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegistrationScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/profile': (context) => const ProfileScreen(),
-      },
     );
   }
 }
